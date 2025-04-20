ab237556302fe92ab1defbbe3a46ae6cee6b8f05
Omar Shehata <oshehata862@gmail.com>
2025-04-20 14:41:30 +0200

    edit createExam page

--- src/components/CreateExams/PdfViewer.jsx
+++ src/components/CreateExams/PdfViewer.jsx
@@ -125,3 +125,5 @@ const PDFViewer = () => {
 };
 
 export default PDFViewer;
+
+
--- src/pages/CreateExam.jsx
+++ src/pages/CreateExam.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import PDFViewer from '../components/PdfViewer';
-
+import PDFViewer from '../components/CreateExams/PdfViewer';
+import {Header}  from '../components/CreateExams/Header';
 function CreateExam() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4 mt-12">
+      <div className="w-full max-w-7xl bg-white rounded-2xl shadow-lg mb-5 mt-1">
+      <Header />
+      </div>
       <div className="w-full max-w-7xl bg-white rounded-2xl shadow-lg p-6">
         <PDFViewer />
       </div>
